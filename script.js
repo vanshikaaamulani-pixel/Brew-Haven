72db082605c2cf124a5262dee5c9047255204b41
Vanshika <vanshikaaamulani@gmail.com>
2026-06-13 23:12:21 +0530

    Add Brew Haven website

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener('click', () => {
+    // Toggle mobile menu visibility //
+    document.body.classList.toggle('show-mobile-menu');
+});
+
+menuCloseButton.addEventListener('click', () => {
+    // Hide mobile menu //
+    document.body.classList.remove('show-mobile-menu');
+});
